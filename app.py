19b8b3a3878f65f141d623fa0c0e7b69fd96a462
Mateusz Lorenc <MALOR@orsted.com>
2022-08-16 14:16:33 +0200

    added functioning multiple ping

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
 
 app = Flask(__name__)
@@ -15,9 +15,10 @@ def output_parser(cmd_output):
     new_output["hostname"] = output_list[1]
     if int(output_list[output_list.index("Received") + 2][0]) > 0:
         new_output["connected"] = True
+        new_output["time"] = int(output_list[output_list.index("Average") + 2][0])
     else:
         new_output["connected"] = False
-    new_output["time"] = int(output_list[output_list.index("Average") + 2][0])
+        new_output["time"] = "n/a"
     return new_output
 
 
@@ -27,13 +28,14 @@ def ping_ip(hostname):
     return jsonify(output_parser(cmd))
 
 
-@app.route('/api/ips/<hostnames>')
-def ping_ips(hostnames):  # put application's code here
+@app.route('/api/ips/', methods=['POST'])
+def ping_ips():  # put application's code here
+    hostnames = request.json.get("hosts")
     results = []
-    for host in hostnames.split(','):
-        cmd = os.popen("ping " + host).read()
-        results.append(cmd)
-    return results
+    for i in hostnames:
+        cmd = os.popen("ping " + i).read()
+        results.append(output_parser(cmd))
+    return jsonify(results)
 
 
 if __name__ == '__main__':
