c31fc0b890309b2fae3590a57da1c3c8a543fcb2
Mateusz Lorenc <MALOR@orsted.com>
2022-08-16 11:54:57 +0200

    added output parsing

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
 import os
-import subprocess
 
 app = Flask(__name__)
 
@@ -10,17 +9,29 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+def output_parser(cmd_output):
+    new_output = {"hostname": "", "connected": "", "time": ""}
+    output_list = cmd_output.split(" ")
+    new_output["hostname"] = output_list[1]
+    if int(output_list[output_list.index("Received") + 2][0]) > 0:
+        new_output["connected"] = True
+    else:
+        new_output["connected"] = False
+    new_output["time"] = int(output_list[output_list.index("Average") + 2][0])
+    return new_output
+
+
 @app.route('/api/ip/<hostname>')
 def ping_ip(hostname):
-    cmd = os.popen("ping -c 4 " + hostname).read()
-    return str(cmd)
+    cmd = os.popen("ping " + hostname).read()
+    return jsonify(output_parser(cmd))
 
 
 @app.route('/api/ips/<hostnames>')
 def ping_ips(hostnames):  # put application's code here
     results = []
     for host in hostnames.split(','):
-        cmd = os.popen("ping -c 4 " + host).read()
+        cmd = os.popen("ping " + host).read()
         results.append(cmd)
     return results
 
